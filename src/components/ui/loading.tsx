5e51c033fc78764c5a31979f4318f8424106bceb
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2024-07-13 23:41:57 +0600

    loader update

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="animate-spin rounded-full size-20 border-t-4 border-b-4 border-primaryColor"></div>
+    <div className="flex items-center justify-center h-[400px]">
+      <div className="animate-spin rounded-full size-10 border-t-4 border-b-4 border-primaryColor"></div>
     </div>
   );
 };
